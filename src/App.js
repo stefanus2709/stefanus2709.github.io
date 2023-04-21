3db4c66f28e602a232e914a43e20ac5af36b22e1
Stefanus Likardi <stefanuslikardi@gmail.com>
2023-04-21 21:46:45 +0700

    [add] home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './component/Navbar';
+import Footer from './component/Footer';
+import HomeContent from './component/Home/HomeContent';
+import HomeProject from './component/Home/HomeProject';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <Navbar />
       </header>
+      <div>
+        <HomeContent />
+      </div>
+      <div className='p-10'>
+        <HomeProject />
+      </div>
+      {/* <Footer /> */}
     </div>
   );
 }
